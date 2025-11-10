c608779e3edca9372161aca5823bbecffd2941ca
0ne0r0ne <mikailbaser0@icloud.com>
2025-11-10 19:16:34 +0300

    Add Rock, Paper, Scissors game
    Implement a simple Rock, Paper, Scissors game that allows the user to play against the computer.

---
+++ rock_paper_scissors.py
@@ -0,0 +1,27 @@
+import random
+options = ["Rock", "Paper", "Scissors"]
+user_options = input("Rock, Paper or Scissors\n")
+computer_options = random.choice(options)
+
+if user_options == computer_options:
+    print("It's a tie!")
+elif user_options == "Rock" and computer_options == "Paper":
+    print("You lost!")
+elif user_options == "Rock" and computer_options == "Scissors":
+    print("You won!")
+elif user_options == "Paper" and computer_options == "Rock":
+    print("You won!")
+elif user_options == "Paper" and computer_options == "Scissors":
+    print("You lost!")
+elif user_options == "Scissors" and computer_options == "Rock":
+    print("You lost!")
+elif user_options == "Scissors" and computer_options == "Paper":
+    print("You won")
+
+print(f"Your choice is {user_options} and computer chocice is {computer_options}")
+
+
+
+
+
+
