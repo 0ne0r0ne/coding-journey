a8a1a01589396fafbabe8f64cfd014daea6520ed
0ne0r0ne <mikailbaser0@icloud.com>
2025-12-31 00:26:27 +0300

    Fix typo in input prompt from 'world' to 'word'

--- NATO-alphabet-translator/main.py
+++ NATO-alphabet-translator/main.py
@@ -5,7 +5,7 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 new_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 print(new_dict)
 
-user_world = input("Enter a world").upper()
+user_world = input("Enter a word").upper()
 
 user_results = [new_dict[i] for i in user_world]
 
