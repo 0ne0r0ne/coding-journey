c5fe2747126a4c3a762329bfcd6eeecfe977126f
0ne0r0ne <mikailbaser0@icloud.com>
2025-12-31 15:37:15 +0300

    Fix labels and variable names in converter UI

--- miles_to_km_converter.py
+++ miles_to_km_converter.py
@@ -21,21 +21,21 @@ user_input.grid(row=0, column=2)
 user_input.config(width=8)
 user_input.insert(END, string="0")
 
-#text
+
 text = Label(text="Miles")
 text.grid(row=0, column=3)
 
-#second entry !!!! it's not entry, it should be text....
 
-user_input_2 = Label(text="0") #==>> instead of miles need variable
+
+user_input_2 = Label(text="0")
 user_input_2.grid(row=1, column=2)
 user_input_2.config(width=8)
 
-#second text
+
 text_2 = Label(text="Km")
 text_2.grid(row=1, column=3)
 
-#text-3
+
 text_3 = Label(text="is equal to")
 text_3.grid(row=1, column=1)
 
