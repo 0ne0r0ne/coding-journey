91f02f115205fadccc9da5e2ee49806e94469caf
0ne0r0ne <mikailbaser0@icloud.com>
2026-01-04 16:04:58 +0300

    Improve file handling and UI layout
    Refactor file handling and update UI elements for password manager.

--- password_manager/main.py
+++ password_manager/main.py
@@ -43,18 +43,38 @@ def save():
             with open("data.json", "r") as data_file:
                 data = json.load(data_file)
         except FileNotFoundError:
-             with open("data.json", "w") as data_file:
-                 json.dump(new_data, data_file, indent=4)
-
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
         else:
             #Updating old data with new data
             data.update(new_data)
 
-        with open("data.json" , "w") as data_file:
-            json.dump(data, data_file, indent=4)
-
+            with open("data.json" , "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get() 
+
+    try:
+        # 2. Adım: Dosyayı okumaya çalış
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Hata", message="Henüz hiç veri dosyası yok.")
+
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"There is no data for {website} ")
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -77,11 +97,13 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=38)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "angela@gmail.com")
+email_entry.focus()
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
@@ -90,5 +112,7 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
